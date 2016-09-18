47d5f656d6ce1c2fdfd9b6dde47e9e1a04b6acbc
Robert F. Dickerson <rfdickerson@gmail.com>
2016-09-17 23:29:12 -0500

    Downgrade Kassandra to BlueSocket 0.10

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ import PackageDescription
 let package = Package(
     name: "Kassandra",
     dependencies: [   
-	.Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 11),
+	.Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 10),
     ]
 )
